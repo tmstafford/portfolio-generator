b85f8d2aa2653602d5c2bd0ee99d54b933bd0e92
tmstafford <tatumstafford1@gmail.com>
2020-11-14 11:42:04 -0600

    inquirer first prompt

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const [name, github] = profileDataArgs;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//    if (err) throw err;
+
+//    console.log('Portfolio complete! Check out index.html to see the output!');
+// });
