1c615daf07613c12aa30b1fd0ec89b46945cc2a2
tmstafford <tatumstafford1@gmail.com>
2020-11-14 15:25:44 -0600

    updated html and featured projects

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -131,18 +134,12 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
+        //console.log(portfolioData);
     });
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// const [name, github] = profileDataArgs;
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//    if (err) throw err;
-
-//    console.log('Portfolio complete! Check out index.html to see the output!');
-// });
